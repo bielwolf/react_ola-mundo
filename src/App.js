fb6d6b9beed65e8d7fc3908c6a5e851206befc58
bielwolf <gabryellobo@gmail.com>
2025-09-24 21:02:53 -0300

    aplicação do react router dom

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Inicio from "./paginas/inicio";
+import SobreMim from "./paginas/SobreMim";
+
+
 function App() {
   return (
-    <h1>Olá Mundo!</h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Inicio/>}/>
+        <Route path="/sobremim" element={<SobreMim/>}/>
+        <Route path="*" element={<div>Página não encontrada</div>}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
